fd870946c7a64c9a426a52e280637c01fbfdf697
siw36 <rhh.klussmann@gmail.com>
2018-10-03 14:05:56 +0200

    ansible user creation and sudoers group permissions

--- node_preparation.sh
+++ node_preparation.sh
@@ -5,23 +5,24 @@ set -x
 # Detect the host os type
 HOSTOS=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
-# Read password for ssh key encryption
-#read -p "Enter passphrase for the ssh key file: " SSHPASS
-
-# Read password for ssh key encryption
-#read -p "Enter Ansible Tower IP address/FQDN: " TOWER
+# Read password for ansible user 
+read -p "Enter password for the ansible user: " ANSIBLEPASS
 
 case $HOSTOS in
     '"CentOS Linux"'|'Fedora')
+        # Create user ansible
+        useradd ansible
+        # Set the password
+        echo $ANSIBLEPASS | passwd ansible --stdin
+        # Allow ansible user to use sudo without password
+        sudo sed -i s/'# %wheel'/'%wheel'/ /etc/sudoers
+        sudo sed -i s/'NOPASSWD: ALL'/'NOPASSWD: ansible'/ /etc/sudoers
         # Update
         sudo yum update -y
         # Install Python modules
         sudo yum install -y python2 python-simplejson
         # Install SELinux modules
         sudo yum install -y libselinux-python
-        # Generate SSH ID and copy to ansible tower
-        #ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N "$SSHPASS"
-        #ssh-copy-id ansible@$TOWER
         echo "Done"
         exit 0
         ;;
