bd11aabd4a482b1c981baaed0ca8b83e0c8ad479
siw36 <rhh.klussmann@gmail.com>
2018-10-03 13:31:26 +0200

    no ssh

--- node_preparation.sh
+++ node_preparation.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+set -x
+
 # Detect the host os type
-$HOSTOS=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
+HOSTOS=$(awk -F= '/^NAME/{print $2}' /etc/os-release)
 
 # Read password for ssh key encryption
-read -p "Enter passphrase for the ssh key file: " SSHPASS
+#read -p "Enter passphrase for the ssh key file: " SSHPASS
 
 # Read password for ssh key encryption
-read -p "Enter Ansible Tower IP address/FQDN: " TOWER
+#read -p "Enter Ansible Tower IP address/FQDN: " TOWER
 
 case $HOSTOS in
     '"CentOS Linux"'|'Fedora')
@@ -18,8 +20,8 @@ case $HOSTOS in
         # Install SELinux modules
         sudo yum install -y libselinux-python
         # Generate SSH ID and copy to ansible tower
-        ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N "$SSHPASS"
-        ssh-copy-id ansible@$TOWER
+        #ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N "$SSHPASS"
+        #ssh-copy-id ansible@$TOWER
         echo "Done"
         exit 0
         ;;
@@ -29,8 +31,8 @@ case $HOSTOS in
         # Install Python modules
         sudo yum install -y python2 python-simplejson
         # Generate SSH ID and copy to ansible tower
-        ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N "$SSHPASS"
-        ssh-copy-id ansible@$TOWER
+        #ssh-keygen -b 2048 -t rsa -f $HOME/.ssh/id_rsa -q -N "$SSHPASS"
+        #ssh-copy-id ansible@$TOWER
         echo "Done"
         exit 0
         ;;
