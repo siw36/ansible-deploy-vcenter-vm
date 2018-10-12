b341d1f91a23bcaece0fb90783dd4c6b8b08676c
Robin Klussmann <32791078+siw36@users.noreply.github.com>
2018-10-12 11:34:15 +0200

    no longer the whole group, now just ansible user

--- prepare_host.sh
+++ prepare_host.sh
@@ -56,9 +56,8 @@ case $HOSTOS in
 	sudo usermod -aG wheel ansible
 	printf ">>>Group set\n\n"
 	# Allow wheel users to use sudo without password
-	printf "***Altering sudoers file to allow the wheel group to use sudo without password confirtmation\n"
-	sudo sed -i -E s/'^%wheel[ \t]ALL=\(ALL\)[ \t]ALL'/'#%wheel ALL=(ALL) ALL'/ /etc/sudoers
-	sudo sed -i -E s/'^# %wheel[ \t]ALL=\(ALL\)[ \t]NOPASSWD: ALL'/'%wheel ALL=(ALL) NOPASSWD: ALL'/ /etc/sudoers
+	printf "***Altering sudoers file to allow the ansible user to use sudo without password confirtmation\n"
+	echo 'ansible ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 	printf ">>>Altering sudoers finished\n\n"
 	printf ">>>Host preparation finished\n>>>Review the output above for errors!\n\n>>>This host can now be added to your Ansible inventory\n>>>Hostname: $FQDN\n"
 	for i in $IP4; do
@@ -108,9 +107,8 @@ case $HOSTOS in
 	usermod -aG sudo ansible
 	printf ">>>Group set\n\n"
 	# Allow sudo users to use sudo without password
-	printf "***Altering sudoers file to allow the sudo group to use sudo without password confirtmation\n"
-	sed -i -E s/'^%sudo[ \t]ALL=\(ALL:ALL\)[ \t]ALL'/'#%sudo ALL=(ALL:ALL) ALL'/ /etc/sudoers
-	echo '%sudo ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
+	printf "***Altering sudoers file to allow the ansible user to use sudo without password confirtmation\n"
+	echo 'ansible ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 	printf ">>>Altering sudoers finished\n\n"
 	printf ">>>Host preparation finished\n>>>Review the output above for errors!\n\n>>>This host can now be added to your Ansible inventory\n>>>Hostname: $FQDN\n"
 	for i in $IP4; do
@@ -164,9 +162,8 @@ case $HOSTOS in
 	usermod -aG sudo ansible
 	printf ">>>Group set\n\n"
 	# Allow sudo users to use sudo without password
-	printf "***Altering sudoers file to allow the sudo group to use sudo without password confirtmation\n"
-	sed -i -E s/'^%sudo[ \t]ALL=\(ALL:ALL\)[ \t]ALL'/'#%sudo ALL=(ALL:ALL) ALL'/ /etc/sudoers
-	echo '%sudo ALL=(ALL:ALL) NOPASSWD: ALL' >> /etc/sudoers
+	printf "***Altering sudoers file to allow the ansible user to use sudo without password confirtmation\n"
+	echo 'ansible ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 	printf ">>>Altering sudoers finished\n\n"
 	printf ">>>Host preparation finished\n>>>Review the output above for errors!\n\n>>>This host can now be added to your Ansible inventory\n>>>Hostname: $FQDN\n"
 	for i in $IP4; do
